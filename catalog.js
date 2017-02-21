357f2618e97f3fadb9b3af1979c9c39877730346
canilacatalin <canila.catalin@gmail.com>
2017-02-21 23:05:08 +0200

    latest
    inca ma chinui

--- catalog.js
+++ catalog.js
@@ -23,28 +23,22 @@
 		$("#addButton").click(function(){
 			
 			
-			addToList();
-			addToTable();
+			addToListnTable();
 			clear();
 			
 		});
+		$(".deleteButton").click(function(){
+		removeCheckedRows('myTable', contactList);
+		refreshTable(contactList);
+		
+	});
+	
  });
 
 	var contactList = [];
-	var firstName = document.getElementById("firstName");
-	var lastName = document.getElementById("lastName");
-	var status = document.getElementById("status");
-	var yearOfBirth = document.getElementById("yearOfBirth");
-	var address = document.getElementById("address");
-	var phoneNumber = document.getElementById("phoneNumber");
-	/*var firstname = $("#firstName").val();
-	var lastname = $("#lastName").val();
-	var status = $("#status").val();
-	var address = $("#address").val();
-	var phonenumber = $("#phoneNumber").val();*/
+	var nr = 0;
 	
-	
-	function Details(firstName, lastName, status, yearOfBirth, address, phoneNumber)
+	function Person(firstName, lastName, status, yearOfBirth, address, phoneNumber)
 	{
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -54,22 +48,38 @@
 		this.phoneNumber = phoneNumber;
 	}
 	
-	function addToList(){
+	function addToListnTable(){
+		
+		nr++;
+		var firstName = document.getElementById("firstName");
+		var lastName = document.getElementById("lastName");
+		var status = document.getElementById("status");
+		var yearOfBirth = document.getElementById("yearOfBirth");
+		var address = document.getElementById("address");
+		var phoneNumber = document.getElementById("phoneNumber");
 		if(firstName.value!="" && lastName.value!="" && status.value!="")
 		{
-			var contact = new Details(firstName.value, lastName.value, status.value, yearOfBirth.value, address.value, phoneNumber.value);
+			var contact = new Person(firstName.value, lastName.value, status.value, yearOfBirth.value, address.value, phoneNumber.value);
 			contactList.push(contact);	
 		}
+		$("#myTable").append('<tr><td id="checkBox"><input type="checkBox"></td><td class="rowID" id="checkBox">' + nr + '</td> <td>' + firstName.value + '</td> <td>' + lastName.value + '</td> <td>' + status.value + 
+		'</td> <td> <input id="edit" type="button" value="Edit" onClick="edit()"> <input id="deleteRow" type="button" value="Delete" onClick="deleteThisRow(this, contactList)"> </td></tr>');
 		
 	}
 		
-	function addToTable(){
-		for( var i in contactList)
+	function refreshTable(list){
+		var number = 1;
+		var Table = document.getElementById("tableBody");
+		Table.innerHTML = "";
+		for( var i in list)
 		{
-			$("#myTable").append('<tr><td id="checkBox"><input type="checkBox"></td><td class="rowID" id="checkBox"></td> <td>' + contactList[i].firstName + '</td> <td>' + contactList[i].lastName + '</td> <td>'
-			+ contactList[i].status + '</td><td> <input id="edit" type="button" value="Edit" onClick="edit()"> <input id="deleteRow" type="button" value="Delete" onClick="deleteThisRow(this)"> </td></tr>');
+			$("#myTable").append('<tr><td id="checkBox"><input type="checkBox"></td><td class="RowID" id="checkBox">' + number + '</td> <td>' + list[i].firstName + '</td> <td>' + list[i].lastName + '</td> <td>'
+			+ list[i].status + '</td><td> <input id="edit" type="button" value="Edit" onClick="edit()"> <input id="deleteRow" type="button" value="Delete" onClick="deleteThisRow(this, contactList)"> </td></tr>');
+		number++;
 		}
+		
 	}
+	
 		
 	function clear(){
 		$('#wrapper').dialog('close');
@@ -78,60 +88,49 @@
 			selectAll[i].value = '';
 		}
 	}
-	/*$("#addButton").click(function(){
-		var firstName = $("#firstName").val();
-		var lastName = $("#lastName").val();
-		var status = $("#status").val();
-		if(firstName!="" && lastName!="" && status!="")
-		{
-		$("#myTable").append('<tr><td id="checkBox"><input type="checkBox"></td><td class="rowID" id="checkBox"></td> <td>' + firstName + '</td> <td>' + lastName + '</td> <td>' + status + 
-		'</td> <td> <input id="edit" type="button" value="Edit" onClick="edit()"> <input id="deleteRow" type="button" value="Delete" onClick="deleteThisRow(this)"> </td></tr>');
-		}
 
-		$('#wrapper').dialog('close');
-		$("#firstName").val("");
-		$("#lastName").val("");
-		$("#status").val("");
-		$("#phoneNumber").val("");
-		$("#yearOfBirth").val("");
-		$("#address").val("");
-		updateNr();
-	});
-	$(".deleteButton").click(function(){
-		removeCheckedRows('myTable');
-		updateNr();
-	});
+
+function removeCheckedRows(tableID, list){
 	
-});
-
-function removeCheckedRows(tableID){
+	
 	var objTable = document.getElementById(tableID).tBodies[0];
 	var rowCount = objTable.rows.length;
 	if(objTable.rows.length > 1)
 	{
-		for( var i = 1; i < rowCount; i++){
+		for( var i = 1; i < rowCount; i++){;
 			var row = objTable.rows[i];
 			var chkbox = row.cells[0].getElementsByTagName('input')[0];
 			if(null!=chkbox && true == chkbox.checked) {
-				objTable.deleteRow(i);
-				rowCount--;
-				i--;
+				alert(i);
+				alert(list[i].firstName);
+				delete list[i];
 			}
+			
 		}
 	}
+	
 }		
+
+
 			
-function deleteThisRow(e){
+function deleteThisRow(e, list){
+	
+	
 	var thisRow = e.parentNode.parentNode.rowIndex;
-	myTable.deleteRow(thisRow);
-	updateNr();
+	var positionInArray = thisRow - 1;
+	alert(positionInArray);
+	alert(list[positionInArray].firstName);
+	delete list[positionInArray];
+	//refreshTable(list);
+	refreshTable(contactList);
+	
 }
 
 function updateNr(){
 	$(".rowID").each(function(i) {
 			$(this).text(i+1);
 		});
-}*/
+}
 			
 			
 			
